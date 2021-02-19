b82ba2caf199cfd3a03316f0b2754ff86bccdccd
Developer-Hadi <hadiuzzamanliton@gmail.com>
2021-02-19 21:30:19 +0600

    Halim template home page complete

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
+    // slider active
+    $(".sliders").owlCarousel({
         'items':1,
         'loop': true,
         'autoplay': true,
     });
+    // Testimonial slide active
+    $(".testimonials").owlCarousel({
+        'items':3,
+        'loop': true,
+        'autoplay': true,
+        'nav': false,
+        'dots': false,
+        'margin': 15,
+        'center': true
+    });
+
+    // counter active
+    $('.counter').counterUp();
   });
